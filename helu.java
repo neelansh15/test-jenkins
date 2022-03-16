4fb7707d0f06c755b1a6af2240945956d868ceec
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-03-16 10:18:20 +0000

    ft: update line in helu java file

--- helu.java
+++ helu.java
@@ -1,6 +1,6 @@
 
 class helu {
     public static void main(String[] args){
-        System.out.println("Helu there!");
+        System.out.println("Helu there! Commiting this line from repl");
     }
 }
